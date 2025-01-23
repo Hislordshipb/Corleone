87265308581b512305b79aedc60f3ccbb2809fa0
Preshiii <94832799+Preshiii@users.noreply.github.com>
2025-01-23 23:50:22 +0100

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -281,7 +281,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f"{files.file_name}"
 
         try:
-            if AUTH_CHANNEL and not await is_subscribed(client, query):
+            links = await is_subscribed(client, query=query)
+            if AUTH_CHANNEL and not len(links) == 0:
                 if clicked == typed:
                     await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                     return
